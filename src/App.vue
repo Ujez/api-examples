2154161ac01dd1fa1a4c3e9ba010a55cfb289fa7
ujez <ujezujez@gmail.com>
2022-02-03 12:57:29 +0100

    small fixes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <h3 class="p-3 text-center">Vue HTTP GET Requests with Fetch</h3>
-    <!-- <get-request/> -->
-   <get-request-async-await/>
+    <get-request/>
+   <!-- <get-request-async-await/> -->
   </div>
 </template>
 
 <script>
-// import GetRequest from "./components/GetRequest.vue";
-import GetRequestAsyncAwait from "./components/GetRequestAsyncAwait.vue";
+import GetRequest from "./components/GetRequest.vue";
+// import GetRequestAsyncAwait from "./components/GetRequestAsyncAwait.vue";
 
 export default {
   name: "app",
   components: {
-   GetRequestAsyncAwait
+    GetRequest,
+  //  GetRequestAsyncAwait
   }
 };
 </script>
