c367757e284507cc59fae48361b58c6ad2df2814
ujez <ujezujez@gmail.com>
2022-02-03 12:53:51 +0100

    small fixes

---
+++ src/components/GetRequest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card text-center m-3">
+    <h5 class="card-header">Simple GET Request</h5>
+    <div class="card-body">Total vue packages: {{totalVuePackages}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "get-request",
+  data() {
+    return {
+      totalVuePackages: null,
+    };
+  },
+  created() {
+    // Simple GET request using fetch
+    fetch("https://api.npms.io/v2/search?q=vue")
+      .then(response => response.json())
+      .then(data => (this.totalVuePackages = data.total));
+  },
+
+};
+</script>
---
+++ src/components/GetRequestAsyncAwait.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card text-center m-3">
+    <h5 class="card-header">GET Request with Async/Await</h5>
+    <div class="card-body">Total vue packages: {{totalVuePackages}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "get-request-async-await",
+  data() {
+    return {
+      totalVuePackages: null
+    };
+  },
+  async created() {
+    // GET request using fetch with async/await
+    const response = await fetch("https://api.npms.io/v2/search?q=vue");
+    const data = await response.json();
+    this.totalVuePackages = data.total;
+  }
+};
+</script>
